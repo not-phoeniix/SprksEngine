61b2786535277973754989073cc4b9e1e67d0a4c
Nikki Murello <nmurello15@gmail.com>
2025-06-19 10:42:10 -0400

    Added new particle system classes

---
+++ Embyr/Particle.cs
@@ -0,0 +1,9 @@
+using Embyr.Scenes;
+using Microsoft.Xna.Framework;
+
+namespace Embyr;
+
+public class Particle : Actor2D {
+    public Particle(Vector2 position, Scene2D scene) : base(position, scene) {
+    }
+}
